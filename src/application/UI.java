da9923304c148f52e11c827a08958839d33060bf
Amanda Pontes <amandapm@globo.com>
2020-07-08 16:55:52 -0300

    CheckMate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -58,10 +58,16 @@ public class UI {
 		printCapturedPieces(captured);
 		System.out.println();
 		System.out.println("Turn: " + chessMatch.getTurn());
+		if(!chessMatch.getCheckMate()) {
 		System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
 		if(chessMatch.getCheck()) {
 			System.out.println("CHECK!");
+		 }
+		} else {
+			System.out.println("CheckMate");
+			System.out.println("Winner: " + chessMatch.getCurrentPlayer());
 		}
+		
 	}
 	
 	
