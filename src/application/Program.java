337c42092491a567154ef63123285cee2ba27746
Amanda Pontes <amandapm@globo.com>
2020-07-12 22:46:49 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,10 +37,15 @@ public class Program {
 				 ChessPiece capturedPiece = chessMatch.perfomChessMove(source, target);
 				 
 				 if(capturedPiece != null) {
-					 
 					 captured.add(capturedPiece);
-					 
 				 }
+				 
+				 if(chessMatch.getPromoted() != null) {
+					 System.out.println("Enter piece for promotion (B/N/R/Q): ");
+					 String type = sc.nextLine();
+					 chessMatch.replacePromotedPiece(type);
+				 }
+				 
 			 }
 			 catch(BoardException e) {
 				 System.out.println(e.getMessage());
